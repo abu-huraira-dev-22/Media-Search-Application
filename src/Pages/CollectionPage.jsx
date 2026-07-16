df4ceefa8bef547853df1fdcf673b9fbf765955e
Abu Huraira Ansari <ahurairaansari047@gmail.com>
2026-07-17 01:21:49 +0500

    updated the code and completed the project

--- src/Pages/CollectionPage.jsx
+++ src/Pages/CollectionPage.jsx
@@ -1,26 +1,38 @@
 import { useDispatch, useSelector } from "react-redux";
 import CollectionCard from "../Components/CollectionCard";
-import {clearCollection} from '../redux/features/collectionSlice'
+import { clearCollection } from "../redux/features/collectionSlice";
 
 const CollectionPage = () => {
   const collection = useSelector((state) => state.collection.items);
-  const dispatch = useDispatch()
-  const clearAll = ()=>{
-    dispatch(clearCollection())
-  }
+  const dispatch = useDispatch();
+  const clearAll = () => {
+    dispatch(clearCollection());
+  };
 
   return (
     <div className="overflow-auto py-6 px-10">
-      <div className="flex justify-between mb-6">
-        <h2 className="text-2xl font-medium">{collection.length>0?'Your Collection': 'Collection is empty'}</h2>
-        <button onClick={()=>{
-          clearAll()
-        }} className="bg-red-600 px-8 py-3 text-lg font-medium rounded active:scale-95 transition cursor-pointer">Clear Collection</button>
-      </div>
-      <div className="flex justify-center w-full flex-wrap gap-6 ">
+      {collection.length > 0 ? (
+        <div className="flex justify-between mb-6">
+          <h2 className="text-2xl font-medium">Your Collection</h2>
+          <button
+            onClick={() => {
+              clearAll();
+            }}
+            className="bg-red-600 px-8 py-3 text-lg font-medium rounded active:scale-95 transition cursor-pointer"
+          >
+            Clear Collection
+          </button>
+        </div>
+      ) : (
+        "Collection is empty"
+      )}
+
+      <div className="flex-start justify-center w-full flex-wrap gap-6 ">
         {collection.map((item, idx) => {
           return (
-          <div key={idx}><CollectionCard item={item} /></div>
+            <div key={idx}>
+              <CollectionCard item={item} />
+            </div>
           );
         })}
       </div>
